ad6be7a60e2bccb6fe343b70c4c78522126473cf
ap20057 <ap20057@ues.edu.sv>
2026-03-06 16:25:41 -0600

    New repository and update submission 5 exercises

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,16 @@
 using System;
-
+//resubmission of the exercise one.
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Ask the user for their name and last name...
+        Console.WriteLine("Hello 2026 CSE-210!. ");
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}. ");
     }
 }
+
